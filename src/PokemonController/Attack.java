bf5c0a5a9b4e6c7b57825f55ee29259b7d3c9d89
Daniel <ferndani00@gmail?.com>
2023-05-07 12:59:25 +0200

    Attack klasse aangemaakt

---
+++ src/PokemonController/Attack.java
@@ -0,0 +1,16 @@
+package PokemonController;
+
+public class Attack {
+
+    private int attackPower;
+    private int pp;
+    private int accuracy;
+
+    public Attack(int attackPower, int pp, int accuracy){
+        this.attackPower = attackPower;
+        this.pp = pp;
+        this.accuracy = accuracy;
+    }
+
+
+}
