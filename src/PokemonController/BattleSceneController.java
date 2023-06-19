48e463ca79bc9d245be1211168f55167d9711aeb
Daniel <ferndani00@gmail.com>
2023-06-19 21:21:48 +0200

    voorlopige battleScene opmaak gemaakt en skeleton in controller

--- src/PokemonController/BattleSceneController.java
+++ src/PokemonController/BattleSceneController.java
@@ -1,4 +1,58 @@
 package PokemonController;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+
 public class BattleSceneController {
+
+    @FXML
+    private Button btnAttack1;
+
+    @FXML
+    private Button btnAttack2;
+
+    @FXML
+    private Button btnAttack3;
+
+    @FXML
+    private Button btnAttack4;
+
+    @FXML
+    private Label lblBattleScene;
+
+    @FXML
+    private Label lblEnemyPokemon;
+
+    @FXML
+    private Label lblPlayerPokemon;
+
+    @FXML
+    private ProgressBar prgsEnemyHP;
+
+    @FXML
+    private ProgressBar prgsYourPokemon;
+
+    @FXML
+    void btnAttack1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnAttack2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnAttack3(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnAttack4(ActionEvent event) {
+
+    }
+
 }
