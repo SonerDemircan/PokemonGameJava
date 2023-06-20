9e4bfe3c5c6d6dfb37ea2cb2d023a987f0ef4fd0
Daniel <ferndani00@gmail.com>
2023-06-20 12:01:23 +0200

    voorlopige BattleSceneController (nog niet klaar)

--- src/PokemonController/BattleSceneController.java
+++ src/PokemonController/BattleSceneController.java
@@ -1,10 +1,14 @@
 package PokemonController;
 
+import PokemonGame.Pokemon;
+import PokemonGame.World;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
+
+import java.util.Scanner;
 
 public class BattleSceneController {
 
@@ -21,6 +25,9 @@ public class BattleSceneController {
     private Button btnAttack4;
 
     @FXML
+    private Button btnStartBattle;
+
+    @FXML
     private Label lblBattleScene;
 
     @FXML
@@ -34,6 +41,56 @@ public class BattleSceneController {
 
     @FXML
     private ProgressBar prgsYourPokemon;
+
+    private World _world;
+
+    private String text = "It's your turn!";
+
+    public BattleSceneController(World world) {
+        this._world = world;
+    }
+
+    public void battle(Pokemon trainerPok, Pokemon enemy) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(trainerPok.getBattleHitPoints() > 0 && enemy.getBattleHitPoints() > 0) {
+            //System.out.println("It's " + trainerPok.name + " turn!");
+            lblBattleScene.setText(text);
+            btnAttack1.setText(trainerPok.getMoveSet()[0].getName() + " -- pp: " + trainerPok.getMoveSet()[0].getPp());
+            System.out.println("pick a move: \n" + "0: " +  trainerPok.getMoveSet()[0].getName() + " -- pp: " + trainerPok.getMoveSet()[0].getPp() + "\n1: " + trainerPok.getMoveSet()[1].getName() + " -- pp: " + trainerPok.getMoveSet()[1].getPp() + "\n2: " + trainerPok.getMoveSet()[2].getName() + " -- pp: " + trainerPok.getMoveSet()[2].getPp() + "\n3: " + trainerPok.getMoveSet()[3].getName()+ " -- pp: " + trainerPok.getMoveSet()[3].getPp());
+            int attackmove = scanner.nextInt();
+            enemy.setBattleHitPoints(-trainerPok.attack(trainerPok,attackmove,enemy));
+
+            if(isPokemonDefeated(enemy)) {
+                System.out.println("You and " + trainerPok.name + " won!");
+                continue;
+            }
+
+            System.out.println("It's " + enemy.name + " turn!");
+            System.out.println("pick a move: \n" + "0: " +  enemy.getMoveSet()[0].getName() + " -- pp: " + enemy.getMoveSet()[0].getPp() + "\n1: " + enemy.getMoveSet()[1].getName() + " -- pp: " + enemy.getMoveSet()[1].getPp() + "\n2: " + enemy.getMoveSet()[2].getName() + " -- pp: " + enemy.getMoveSet()[2].getPp() + "\n3: " + enemy.getMoveSet()[3].getPp()+ " -- pp: " + enemy.getMoveSet()[3].getPp() );
+            attackmove = scanner.nextInt();
+            trainerPok.setBattleHitPoints(-enemy.attack(enemy,attackmove,trainerPok));
+
+            if(isPokemonDefeated(trainerPok)) {
+                System.out.println("the enemy and " + enemy.name + " won!");
+                continue;
+            }
+
+            //System.out.println(trainerPok.name + " HP: " + trainerPok.maxHitPoints + "/" + trainerPok.battleHitPoints);
+            //System.out.println(enemy.name + " HP: " + enemy.maxHitPoints + "/" + enemy.battleHitPoints);
+            System.out.println();
+        }
+    }
+
+    public boolean isPokemonDefeated(Pokemon pokemon) {
+        boolean defeat = false;
+        if(pokemon.getBattleHitPoints() < 1) {
+            defeat = true;
+        }
+        return defeat;
+    }
+
 
     @FXML
     void btnAttack1(ActionEvent event) {
@@ -54,5 +111,11 @@ public class BattleSceneController {
     void btnAttack4(ActionEvent event) {
 
     }
+
+    @FXML
+    void btnStartBattle(ActionEvent event) {
+        battle(_world.getPokemon().get(5), _world.getPokemon().get(10));
+    }
+
 
 }
